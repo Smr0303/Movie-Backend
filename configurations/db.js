013e8dd0031bf4448b2507bd273bd8187d8f8a90
Samarjeet Mohite <samarmohite7@gmail.com>
2021-06-10 00:56:13 +0530

    updated

--- configurations/db.js
+++ configurations/db.js
@@ -1,6 +1,6 @@
 const {Client}=require("pg");
 console.log(process.env.User);
-const client = new client({
-   url: process.env.User
-});
+const client = new Client(
+   process.env.User
+);
 module.exports=client;
