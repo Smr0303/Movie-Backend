654a5bc8f66658dbc91fc8799452dd01ed94bf59
Samarjeet Mohite <samarmohite7@gmail.com>
2021-06-10 02:28:42 +0530

    final commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,58 +1,61 @@
-const bcrypt=require('bcrypt');
-const jwt=require('jsonwebtoken');
-const client =require('../configurations/db');
-exports.signUp=(req,res)=>{
-    const { name,email,password,contact}=req.body;
-    client.query(`SELECT * FROM users where email=${email};`)
-    .then((data)=>{
-        const exists=data.rows;
-    if(exists.length!==0){
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const client = require("../configurations/db");
+exports.signUp = (req, res) => {
+  const { name, email, password, contact } = req.body;
+  client
+    .query(`SELECT * FROM users where email='${email}';`)
+    .then((data) => {
+      const exists = data.rows;
+      if (exists.length !== 0) {
         res.status(500).json({
-            error:"User alerady exists",
+          error: "User alerady exists",
         });
-    }
-    else{
-        bcrypt.hash(password ,10 , (err,hash)=>{
-            if(err){
-                res.status(500).json({
-                    message:"Internal server error",
-                });
-            }
-            const user={
-                name,
-                email,
-                password:hash,
-                contact,
-            };
-            const token= jwt.sign(
-                {
-                    "alg": "HS256",
-                    "typ": "JWT"
-                  },
-                  {
-                email:email,
+      } else {
+        bcrypt.hash(password, 10, (err, hash) => {
+          if (err) {
+            console.log(err);
+            res.status(500).json({
+              message: "Internal server error",
+            });
+          }
+          const user = {
+            name,
+            email,
+            password: hash,
+            contact,
+          };
+          const token = jwt.sign(
+            {
+              alg: "HS256",
+              typ: "JWT",
+            },
+            {
+              email: email,
             },
             process.env.SECRET_KEY
-            );
-            client.query(`INSERT INTO users (name , email, password ,contact) VALUES('${user.name}','${user.email},'${user.password}','${user.contact}');`)
-            .then((data)=>{
-                res.status(200).json({
-                    message:"User added succesfully!!!!",
-                    token:token,
-});
-}).catch((err)=>{
-    res.status(500).json({
-        error:"Servor error try after sometime",
-
+          );
+          client
+            .query(
+              `INSERT INTO users (name , email, password ,contact) VALUES('${user.name}','${user.email},'${user.password}','${user.contact}');`
+            )
+            .then((data) => {
+              res.status(200).json({
+                message: "User added succesfully!!!!",
+                token: token,
+              });
+            })
+            .catch((err) => {
+              res.status(500).json({
+                error: "Servor error try after sometime",
+              });
+            });
+          });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+        error: "Servor error try after sometime",
+      });
     });
-});
-}
-);
-    }
-}).catch((err)=>{
-    res.status(500).json({
-
-        error:"Servor error try after sometime",
-    })
-});
-}
+};
