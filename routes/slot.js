fa26f9489e36718c395277d66f1326c7e9b45ab9
Samarjeet Mohite <samarmohite7@gmail.com>
2021-07-01 23:30:09 +0530

    yes

--- routes/slot.js
+++ routes/slot.js
@@ -3,8 +3,8 @@ const routes=express.Router();
 const{checkToken}=require('../middleware/authmiddleware');
 
 const{showslots,showavailable,showseats}=require('../controllers/slot');
-routes.post("/show_slots",checkToken,showslots);
-routes.post("/show_available",checkToken,showavailable);
-routes.post("/show_seats",checkToken,showseats);
+routes.post("/show_slots",showslots);
+routes.post("/show_available",showavailable);
+routes.post("/show_seats",showseats);
 
 module.exports=routes;
